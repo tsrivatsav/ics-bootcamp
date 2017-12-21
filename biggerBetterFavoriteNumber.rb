be61695e215459e93ee0b90022af7fd1210997a5
Tejas Srivatsav <34287173+tsrivatsav@users.noreply.github.com>
2017-12-21 09:23:44 -0500

    Add files via upload

---
+++ biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+number = gets.chomp
+newNumber = number.to_i + 1
+puts 'Then you should consider ' + newNumber.to_s + ' as a bigger and better favorite number!'
