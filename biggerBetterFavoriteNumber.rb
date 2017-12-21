a9c9b3a95dbc633861ae560fea845932fbe2acb4
Tejas Srivatsav <34287173+tsrivatsav@users.noreply.github.com>
2017-12-21 09:41:33 -0500

    Delete biggerBetterFavoriteNumber.rb

--- biggerBetterFavoriteNumber.rb
+++
@@ -1,4 +0,0 @@
-puts 'What is your favorite number?'
-number = gets.chomp
-newNumber = number.to_i + 1
-puts 'Then you should consider ' + newNumber.to_s + ' as a bigger and better favorite number!'
