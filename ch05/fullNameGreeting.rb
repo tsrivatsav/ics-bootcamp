786eb7f744a23a5238f729ccd56ac8b4f1a9baff
Tejas Srivatsav <34287173+tsrivatsav@users.noreply.github.com>
2017-12-21 09:44:22 -0500

    Add files via upload

---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'What is your first name?'
+first = gets.chomp
+puts 'What is your middle name?'
+middle = gets.chomp
+puts 'What is your last name?'
+last = gets.chomp
+puts 'Hello, it\'s nice to meet you ' + first + ' ' + middle + ' ' + last + '.'
